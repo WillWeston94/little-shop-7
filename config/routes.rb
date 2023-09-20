2835a41ce1271e05ed3754fde1b841ec155eba11
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-19 23:01:20 -0500

    updat the admin/invoices show view to properly display selctor and button, as well as routes update and controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,25 +12,23 @@ Rails.application.routes.draw do
   get "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#show"
   patch "/merchants/:merchant_id/items/:item_id", to: "merchant_items#update"
 
-
   get "/items/:id", to: "items#show"
   get "/items/:id/edit", to: "items#edit"
   patch "/items/:id", to: "items#update"
 
-
-  namespace :admin, path: '/admin' do
-    get '', to: 'dashboard#index', as: 'dashboard'
+  namespace :admin, path: "/admin" do
+    get "", to: "dashboard#index", as: "dashboard"
 
     resources :merchants do
       member do
         put :disable_enable, to: "merchants#disable_enable", as: :disable_enable
       end
     end
-    
+
     resources :invoices, only: [:show]
   end
 
   get "/admin/invoices", to: "admin/invoices#index"
   get "/admin/invoices/:id", to: "admin/invoices#show"
+  patch "/admin/invoices/:id", to: "admin/invoices#update"
 end
-
